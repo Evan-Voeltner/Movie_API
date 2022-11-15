e48da5feffa151df16a5ca255da00448eb532627
Evan Voeltner <evanvoeltner@gmail.com>
2022-11-15 09:31:41 -0600

    Initial commit

---
+++ src/main/java/com/dcc/moive_api/MoiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.dcc.moive_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcc/moive_api/MoiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcc.moive_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
