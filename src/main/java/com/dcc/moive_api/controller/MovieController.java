35bd9de664811e824d3bdbe2a54c56fba9e7bef8
Evan Voeltner <evanvoeltner@gmail.com>
2022-11-15 11:35:40 -0600

    feat: User can make an API call to get all movies + a movie by id or gnere

--- src/main/java/com/dcc/moive_api/controller/MovieController.java
+++ src/main/java/com/dcc/moive_api/controller/MovieController.java
@@ -3,9 +3,9 @@ package com.dcc.moive_api.controller;
 import com.dcc.moive_api.data.Movie;
 import com.dcc.moive_api.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MovieController{
@@ -16,5 +16,20 @@ public class MovieController{
     @PostMapping("/addMovie")
     public Movie addMovie(@RequestBody Movie movie){
         return movieService.saveMovie(movie);
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> findAllMovies(){
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/movies/{id}")
+    public Movie findMovieById(@PathVariable Integer id){
+        return movieService.getById(id);
+    }
+
+    @GetMapping("/findByGenre/{genre}")
+    public List<Movie> findMovieByGenre(@PathVariable String genre){
+        return movieService.getByGenre(genre);
     }
 }
--- src/main/java/com/dcc/moive_api/repository/MovieRepository.java
+++ src/main/java/com/dcc/moive_api/repository/MovieRepository.java
@@ -3,5 +3,8 @@ package com.dcc.moive_api.repository;
 import com.dcc.moive_api.data.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    public List<Movie> findByGenre(String genre);
 }
